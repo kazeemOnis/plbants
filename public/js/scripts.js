eefda258ef468bdea88b80056fe976a3154b43b9
Kazeem Onisarotu <konisarotu@yahoo.com>
2018-01-10 18:06:47 +0000

    error fixed

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,21 +1,7 @@
 $(document).ready(function(){
-    // var $table = $('#positions');
-    var team = ['Arsenal', 'Bourenmouth', 'Brighton', 'Burnley', 'Chelsea',
-                'Crystal Palace', 'Everton', 'Huddersfield', 'Leicester',
-                'Liverpool', 'Manchester City', 'Manchesterter United', 'Newcastle United',
-                'Southampton', 'Stoke City', 'Swansea', 'Tottenham', 'Watford', 'West Bromwich Albion',
-<<<<<<< HEAD
-                'West Ham United' ];
-
-=======
-                'West Ham United'];
-                console.log(team);
-
-                var u;
->>>>>>> 2fb62f61f59dc2385007d2fe691b5b5726d2f8e4
     $.ajax({
         type: 'GET',
-        url: 'api/teams',///team[0,1,2,3,4,5,6,7,8,9],
+        url: 'api/teams',
         success: function(team){
             function table_data(image,count,name,played,wins,loss,draw,goals,conceded,diff,points) {
                 var position = count;
@@ -28,18 +14,15 @@ $(document).ready(function(){
                 var goalsAgainst = conceded;
                 var goalsDiff = diff;
                 var points = points;
-<<<<<<< HEAD
                 var tbody = $('<tbody></tbody>');
                 var td = $('<td></td>').attr('id','col').append(position);
                 var td1 = $('<td></td>').attr('id','col1').append(club);
-=======
                 var image = image;
                 var img = $('<img>').attr('src',image);
                 img.css('width','20px');
                 var tbody = $('<tr></tr>');
                 var td = $('<td></td>').attr('id','col').append(position);
                 var td1 = $('<td></td>').attr('id','col1').append(img).append(club);
->>>>>>> 2fb62f61f59dc2385007d2fe691b5b5726d2f8e4
                 var td2 = $('<td></td>').attr('id','col2').append(played);
                 var td3 = $('<td></td>').attr('id','col3').append(wins);
                 var td4 = $('<td></td>').attr('id','col4').append(loss);
@@ -52,8 +35,6 @@ $(document).ready(function(){
                 tbody.attr('id','body');
                 tbody.append(td,td1,td2,td3,td4,td5,td6,td7,td8,td9);
             }
-<<<<<<< HEAD
-=======
             function sortTable() {
                 var table, rows, switching, i, x, y, shouldSwitch;
                 table = document.getElementById("positions");
@@ -76,42 +57,13 @@ $(document).ready(function(){
                     }
                 }
             }
->>>>>>> 2fb62f61f59dc2385007d2fe691b5b5726d2f8e4
             for(var i in team){
                 var diff = parseInt(team[i].stats.goals) - parseInt(team[i].stats.conceded);
                 table_data(team[i].image,team[i].stats.position,team[i].name,team[i].stats.played,team[i].stats.wins,
                     team[i].stats.loss,team[i].stats.draws,team[i].stats.goals,
                     team[i].stats.conceded,diff,team[i].stats.points);
             }
-<<<<<<< HEAD
-            for(var i =1; i<=20; i++){
-                td.append(i);
-            }
-            for(var i in team){
-                var diff = team[i].stats.goals - team[i].stats.conceded;
-                for(var x=1; x<=20; x++){
-                    if(parseInt(td.text()) == team[i].stats.pos){
-                        table_data(team[i].image,team[i].stats.position,team[i].name,team[i].stats.played,team[i].stats.wins,
-                    team[i].stats.loss,team[i].stats.draws,team[i].stats.goals,
-                    team[i].stats.conceded,diff,team[i].stats.points);
-                    }
-                }
-            }
+            sortTable();
         }
     });
-=======
-        sortTable();
-        }
-    })
-
-
-
-    function sortPoints(a,b)
-    {
-        if (a < b) return 1;
-        if (a > b) return -1;
-        return 0;
-    }
->>>>>>> 2fb62f61f59dc2385007d2fe691b5b5726d2f8e4
-});
-
+}); 
